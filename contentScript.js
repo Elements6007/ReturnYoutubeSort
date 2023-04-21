6d1b7dbafdc2fdde9268b30a3ceda3cb4245c049
John Quigley <Elements6007@gmail.com>
2023-04-20 23:10:31 -0400

    Add load screen (unstable)
    added simple load screen. does not work with all channels due to video count being sampled from video count including live streams

--- contentScript.js
+++ contentScript.js
@@ -105,6 +105,7 @@
   };
 
   const buttonPressed = async () => {
+    loadAnimation();
     if (addbutton) {
     latest = document.querySelectorAll("#chips")[1].getElementsByClassName("style-scope ytd-feed-filter-chip-bar-renderer")[0];
     } else {
@@ -140,6 +141,50 @@
     } else {
       scriptStop();
     }
+  }
+   
+  const loadAnimation = async () => {
+    var Interval = 500;
+  
+    const videosDiv = document.getElementById("contents");
+    const primary = document.getElementById("primary");
+    const renderer = document.getElementsByClassName("style-scope ytd-two-column-browse-results-renderer")[1];
+
+    const videoNum = document.getElementById("videos-count").getElementsByClassName("style-scope yt-formatted-string")[0].innerHTML;
+    var numdiv = document.querySelectorAll("#content .style-scope ytd-rich-item-renderer").length;
+    
+    videosDiv.style.position = "relative";
+    videosDiv.style.zIndex = "-1";
+    
+    renderer.style.position = "relative";
+    document.body.style.overflowX = "hidden";
+    ac = document.createElement('div');
+    ac.id = "ac";
+    ac.style.position = "absolute";
+    ac.style.top = "600px";
+    ac.style.height = "900px";
+    ac.style.width = "70%";
+    ac.style.backgroundColor = "#0f0f0f";
+    ac.style.color = "#FFFFFF";
+    ac.style.fontSize = "large";
+    ac.innerHTML = "0/" + videoNum;
+    ac.style.padding = "top: 5%"
+    ac.style.zIndex = "10";
+    ac.style.textAlign = "center"
+    primary.append(ac);
+
+    var refreshInterval = setInterval(function() {
+    numdiv = document.querySelectorAll("#content .style-scope ytd-rich-item-renderer").length;
+    console.log("Loading...")
+    ac.innerHTML = numdiv + "/" + videoNum;
+    if (videoNum == numdiv){
+      console.log(videoNum, numdiv);
+      videosDiv.style.zIndex = "0";
+      ac.style.display = "none";
+      clearInterval(refreshInterval);
+      }
+    }, Interval);
+
   }
 
   chrome.runtime.onMessage.addListener((obj, sender, response) => {
